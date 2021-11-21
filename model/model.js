91f39c7ad1e04d4fbfd3b2315096d9ebb72b4cd6
hungprovip0089 <71562654+hungprovip0089@users.noreply.github.com>
2021-11-21 16:35:03 +0700

    Update model.js

--- model/model.js
+++ model/model.js
@@ -4,10 +4,10 @@ const Order = require("./order.model.js");
 const Request = require("./request.model.js");
 const Receipt = require("./receipt.model.js");
 const Table = require("./table.model.js");
-=======
+
 const Server = require("./server.model.js")
 const Food = require("./food.model.js");
->>>>>>> 910f2b9860331684c5090d40467fa3090886a495
+
 
 module.exports.createFood = async (food) =>{
     return new Promise((resolve,reject)=>{
@@ -20,7 +20,7 @@ module.exports.createFood = async (food) =>{
     });
 };
 
-<<<<<<< HEAD
+
 module.exports.createEmployee = async (employee) => {
     return new Promise((resolve,reject)=>{
         Employee.insertMany(employee, function(err){
@@ -28,14 +28,13 @@ module.exports.createEmployee = async (employee) => {
 module.exports.createServer = async (server) => {
     return new Promise((resolve,reject)=>{
         Server.insertMany(server, function(err){
->>>>>>> 910f2b9860331684c5090d40467fa3090886a495
             if(err){
                 console.log(err);
                 resolve(-1);
             }
         })
     });
-<<<<<<< HEAD
+
 };
 
 module.exports.createOrder = async (order) => {
@@ -94,4 +93,4 @@ module.exports.createTable = async (table) =>{
 }
 =======
 };
->>>>>>> 910f2b9860331684c5090d40467fa3090886a495
+
